52053209142758c4777ac1373e71d6505c8df211
Prateek R Patil <ppatil@student.42.us.org>
2016-10-10 17:28:49 -0700

    Added header with macros for ANSI color codes.
    * Merp
    
    [I may not be a smart dog, but I know what roadkill is]

---
+++ ft_ansi.h
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ansi.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ppatil <ppatil@student.42.us.org>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/10/10 16:26:45 by ppatil            #+#    #+#             */
+/*   Updated: 2016/10/10 16:26:45 by ppatil           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_ANSI_H
+# define FT_ANSI_H
+
+# define ANSI_RESET "\033[0m"
+
+# define ANSI_F_RED "\033[31m"
+# define ANSI_F_BRED "\033[31;1m"
+# define ANSI_F_GREEN "\033[32m"
+# define ANSI_F_BGREEN "\033[32;1m"
+# define ANSI_F_YELLOW "\033[33m"
+# define ANSI_F_BYELLOW "\033[33;1m"
+# define ANSI_F_BLUE "\033[34m"
+# define ANSI_F_BBLUE "\033[34;1m"
+# define ANSI_F_MAGENTA "\033[35m"
+# define ANSI_F_BMAGENTA "\033[35;1m"
+# define ANSI_F_CYAN "\033[36m"
+# define ANSI_F_BCYAN "\033[36;1m"
+# define ANSI_F_BLACK "\033[30m"
+# define ANSI_F_BBLACK "\033[30;1m"
+# define ANSI_F_WHITE "\033[37m"
+# define ANSI_F_BWHITE "\033[37;1m"
+
+# define ANSI_B_RED "\033[41m"
+# define ANSI_B_BRED "\033[41;1m"
+# define ANSI_B_GREEN "\033[42m"
+# define ANSI_B_BGREEN "\033[42;1m"
+# define ANSI_B_YELLOW "\033[43m"
+# define ANSI_B_BYELLOW "\033[43;1m"
+# define ANSI_B_BLUE "\033[44m"
+# define ANSI_B_BBLUE "\033[44;1m"
+# define ANSI_B_MAGENTA "\033[45m"
+# define ANSI_B_BMAGENTA "\033[45;1m"
+# define ANSI_B_CYAN "\033[46m"
+# define ANSI_B_BCYAN "\033[46;1m"
+# define ANSI_B_BLACK "\033[40m"
+# define ANSI_B_BBLACK "\033[40;1m"
+# define ANSI_B_WHITE "\033[47m"
+# define ANSI_B_BWHITE "\033[47;1m"
+
+#endif
